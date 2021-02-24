68353b341170843d126e08e0424b31ee0df39c7c
leo13200006 <laukikchavan101@gmail.com>
2021-02-24 11:44:43 +0530

    first commit

---
+++ src/app/borad/borad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-borad',
+  templateUrl: './borad.component.html',
+  styleUrls: ['./borad.component.scss']
+})
+export class BoradComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
